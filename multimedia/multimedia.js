57e0160e357be7d3396d9ed5f1cb2d42dc32a70c
alakbar97 <alakbarsa@code.edu.az>
2018-12-12 03:03:24 +0400

    demo clip site created by usin flex and multimedia

---
+++ multimedia/multimedia.js
@@ -0,0 +1,22 @@
+var v=document.querySelector("video");
+var a=document.querySelector("audio")
+function sp() {
+    v.src="spider.mp4"
+    v.play()
+    a.pause()
+}
+function ty() {
+    v.src="toystory.mp4"
+    v.play()
+    a.pause()
+}
+function gr() {
+    v.src="grinch.mp4"    
+    v.play()
+    a.pause()
+}
+function lk() {
+    v.src="lion.mp4"
+    v.play()
+    a.pause()
+}
